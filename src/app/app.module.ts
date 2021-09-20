e160eae3e7d5dc2e9ca3fe03bca8a223846b7d97
Thomas Liu <s192287@student.hb.se>
2021-09-20 22:49:23 +0200

    Add markup and styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputPsKwComponent } from './input-ps-kw/input-ps-kw.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputPsKwComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/input-ps-kw/input-ps-kw.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-ps-kw',
+  templateUrl: './input-ps-kw.component.html',
+  styleUrls: ['./input-ps-kw.component.css']
+})
+export class InputPsKwComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
